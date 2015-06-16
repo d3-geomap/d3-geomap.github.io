b005f04e522ca36a31fb3f6f6c7aa5cb550fe929
Ramiro Gómez <code@ramiro.org>
2015-06-16 17:21:28 +0200

    deploy

--- js/maps/choropleth/us-states.js
+++ js/maps/choropleth/us-states.js
@@ -3,7 +3,8 @@ var map = d3.geomap.choropleth()
     .projection(d3.geo.albersUsa)
     .column('2012')
     .unitId('fips')
-    .scale(1000);
+    .scale(1000)
+    .legend(true);
 
 d3.csv('/data/venture-capital.csv', function(error, data) {
     d3.select('#map')
