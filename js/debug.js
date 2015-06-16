e5c284bd95340c9797d8ada0b3fd0876e7fb9a17
Ramiro Gómez <code@ramiro.org>
2015-06-16 16:41:02 +0200

    deploy

--- js/debug.js
+++ js/debug.js
@@ -1,16 +1,10 @@
-var map = d3.geomap.choropleth()
+var map = d3.geomap()
     .geofile('/d3-geomap/topojson/countries/USA.json')
     .projection(d3.geo.albersUsa)
-    .column('2012')
-    .unitId('fips')
     .scale(1000)
-    .legend(true)
     .postUpdate(function() {
         map.svg.selectAll('.unit').style('fill', 'red');
     });
 
-d3.csv('/data/venture-capital.csv', function(error, data) {
-    d3.select('#map')
-        .datum(data)
-        .call(map.draw, map);
-});
+d3.select('#map')
+    .call(map.draw, map);
