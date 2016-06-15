959cc1f1464a0746bec3db425b94abb3ea5cacae
Ramiro Gómez <code@ramiro.org>
2016-06-15 12:27:32 +0200

    deploy

---
+++ js/maps/choropleth/custom-domain.js
@@ -0,0 +1,13 @@
+var map = d3.geomap.choropleth()
+    .geofile('/d3-geomap/topojson/world/countries.json')
+    .colors(['green','red', 'grey'])
+    .column('1800')
+    .domain([0, 1])
+    .legend(false)
+    .unitId('Country');
+
+d3.csv('/data/custom-domain.csv', function(error, data) {
+    d3.select('#map')
+        .datum(data)
+        .call(map.draw, map);
+});
